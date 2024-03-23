8f642fd6df7f828c716b3efd1bb3d2b9c85bcabd
Dima Hutsuliak <dima.hutsuliak@mobimeo.com>
2024-03-23 17:48:01 +0100

    Add ABIContractBreakerClient

---
+++ Sources/ABIContractBreakerClient/ABIContractBreakerClient.swift
@@ -0,0 +1,10 @@
+import Foundation
+import ABIContractBreaker
+
+public struct ABIContractBreakerClient {
+
+    public static func modifiedShape() -> Shape  {
+        Shape.rect(w: 10, h: 10)
+    }
+
+}
