02750167f6a960d30aefdb442f17d18ddbbe54f0
Dima Hutsuliak <dima.hutsuliak@mobimeo.com>
2024-03-23 17:52:04 +0100

    Update EntryPoint

--- Sources/Main/EntryPoint.swift
+++ Sources/Main/EntryPoint.swift
@@ -4,10 +4,18 @@ import ABIContractBreakerClient
 @main
 public struct EntryPoint {
 
+    // ABIContractBreakerClient Implementation
+    //
+    // public struct ABIContractBreakerClient {
+    //
+    // public static func modifiedShape() -> Shape  {
+    //    Shape.rect(w: 10, h: 10)
+    // }
+    // }
     static public func main() {
-        // Should be: Shape.rect(w: 10, h: 10)
+        // Must be: `Shape.rect(w: 10, h: 10)`
+        // If it returns `circle(radius: 10)` it means the ABI contract was broken
+        // and it blew up at run time.
         print(ABIContractBreakerClient.modifiedShape())
-
-        print("Hey 123")
     }
 }
